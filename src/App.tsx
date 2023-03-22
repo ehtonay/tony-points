f1bbe84ab2dc2bbc1ad6ee22bb599db0fb265154
Tony <anthonyrtomarchio@gmail.com>
2023-03-21 23:14:59 -0400

    Added more logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ type TaxBracket = {
   min: number;
   max?: number;
   rate: number;
+  taxesPaid?: number;
 }
 type TaxResponse = {
   tax_brackets: TaxBracket[]
@@ -14,13 +15,12 @@ type TaxResponse = {
 export const App = () => {
   const [taxYear, setTaxYear] = useState('');
   const [salary, setSalary] = useState('');
+  const [taxBrackets, setTaxBrackets] = useState([]);
 
   // TODO: move to const file
   const BASE_URL = 'http://localhost:5000/tax-calculator/brackets';
 
   const handleSubmit = async () => {
-    // TODO: make sure inputs are valid numbers
-    let totalTaxes = 0.00;
     const income = parseInt(salary);
 
     await fetch(`${BASE_URL}/${taxYear}`)
@@ -32,28 +32,49 @@ export const App = () => {
         res.tax_brackets?.forEach((taxBracket) => {
           if (income > taxBracket.min) {
             if (!taxBracket.max) {
-              totalTaxes += (income - taxBracket.min) * taxBracket.rate;
+              taxBracket.taxesPaid = (income - taxBracket.min) * taxBracket.rate;
             } else {
               if (income > taxBracket.max) {
-                totalTaxes += (taxBracket.max - taxBracket.min) * taxBracket.rate;
+                taxBracket.taxesPaid = (taxBracket.max - taxBracket.min) * taxBracket.rate;
               } else {
-                totalTaxes += (income - taxBracket.min) * taxBracket.rate;
+                taxBracket.taxesPaid = (income - taxBracket.min) * taxBracket.rate;
               }
             }
           }
         })
-        console.log('Total taxes', totalTaxes);
-        // TODO:  display total taxes, and amount paid for each bracket
-        // Spread this value onto existing response object values
+        const sortedBrackets: TaxBracket[] = res.tax_brackets.sort((a, b) => {
+          return a.min < b.min ? -1 : 1;
+        });
+        console.log('sortedBrackets', sortedBrackets)
+        setTaxBrackets(sortedBrackets);
     });
   }
 
+  let totalTaxesPaid = 0;
+  const taxBracketsView = taxBrackets.map(bracket => {
+    if (bracket.taxesPaid) totalTaxesPaid += bracket.taxesPaid;
+    return (
+      <div key={bracket.min}>
+        <div>${bracket.min} - ${bracket.max}</div>
+        <div>Taxes paid: {bracket.taxesPaid}</div>
+      </div>
+    )
+  });
+
   /** TODO:
+   *    - display total taxes, and amount paid for each bracket
    *    - loading state, form focus, pretty UI/responsiveness
    *    - unit tests
+   *    - keyboard accessibility
    */
 
-  return (
+  return taxBrackets.length > 0 ? (
+    <>
+      Your salary: ${salary} <br/>
+      {taxBracketsView}
+      Total taxes paid: ${totalTaxesPaid}
+    </>
+  ) : (
     <>
       <h1>Marginal Tax Bracket Calculator</h1>
         <input type="text" onChange={e => setTaxYear(e.target.value)} placeholder="Please enter a tax year" />
