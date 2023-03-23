0e2d490a04bffc5205fc6e0117f54f8d9f39522b
Tony <anthonyrtomarchio@gmail.com>
2023-03-23 10:02:20 -0400

    Update unit test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {act} from 'react-dom/test-utils';
+import { act } from 'react-dom/test-utils';
 import * as ReactDOM from "react-dom";
 import { App } from "../App";
 
@@ -10,7 +10,18 @@ describe('App', function () {
         act(() => {
             ReactDOM.render(<App />, container);
         })
+
+        // Basic state element assertions and snapshot
         const header = container.querySelector('h1');
-        expect(header.textContent).toBe("Hello world React! Num: 191")
+        expect(header.textContent).toBe("Marginal Tax Bracket Calculator")
+        expect(container).toMatchSnapshot();
+
+        // Simulate change on input elements to add tax year and salary
+
+        // Simulate click on submit button
+
+        // Mock fetch to return valid data and assert elements and snapshot
+
+        // Mock fetch to return invalid data and assert elements and snapshot
     });
 });
